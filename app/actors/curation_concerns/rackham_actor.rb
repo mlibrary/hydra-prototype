04f582f8bda0205aa955ee116afd57ef527afd43
Nabeela Jaffer <njaffer@umich.edu>
2015-10-05 10:33:34 -0400

    Add work

---
+++ app/actors/curation_concerns/rackham_actor.rb
@@ -0,0 +1,7 @@
+# Generated via
+#  `rails generate curation_concerns:work Rackham`
+module CurationConcerns
+  class RackhamActor < CurationConcerns::BaseActor
+    include ::CurationConcerns::WorkActorBehavior
+  end
+end
--- config/initializers/curation_concerns_config.rb
+++ config/initializers/curation_concerns_config.rb
@@ -1,2 +1,4 @@
 CurationConcerns.configure do |config|
+  # Injected via `rails g curation_concerns:work Rackham`
+  config.register_curation_concern :rackham
 end
