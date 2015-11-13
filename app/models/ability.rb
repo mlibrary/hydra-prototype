546d4f7143544df9a8c90b75f7fa48eaa5c7846a
Nabeela Jaffer <njaffer@umich.edu>
2015-11-13 11:25:21 -0500

    admin can view the lease screen

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,21 +8,25 @@ class Ability
   def custom_permissions
     if current_user.admin?
       can [:create, :show, :add_user, :remove_user, :index, :edit, :update, :destroy], Role
+      can [:create], Collection
       can [:discover], Hydra::AccessControls::Embargo
+      can [:discover], Hydra::AccessControls::Lease
+      can [:create], [ CurationConcerns.config.curation_concerns ]
+      can [:destroy], ActiveFedora::Base
     end
 
     # Limits deleting objects to a the admin user
     #
-    if current_user.admin?
-      can [:destroy], ActiveFedora::Base
-    end
+    #if current_user.admin?
+    #  can [:destroy], ActiveFedora::Base
+    #end
 
-    if current_user.has_role?('collection.manager') or current_user.admin?
+    if current_user.has_role?('collection.manager')
       # can [:create, :show, :index, :edit, :update, :destroy], Collection
       can [:create], Collection
     end
 
-    if current_user.has_role?('collection.depositor') or current_user.has_group_role?('collection.depositor') or current_user.admin?
+    if current_user.has_role?('collection.depositor') or current_user.has_group_role?('collection.depositor')
       # can [:create, :show, :index, :edit, :update, :destroy], [ CurationConcerns.configuration.curation_concerns ]
       can [:create], [ CurationConcerns.config.curation_concerns ]
       # can [:show], Collection
